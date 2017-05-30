f13141f78281f524aab4c58bb8174eab6a6b3d28
Jonathan Bertoldi <jonathanbertoldi7@gmail.com>
2017-05-30 20:27:23 -0300

    Lista de Eventos

--- src/pages/Eventos.js
+++ src/pages/Eventos.js
@@ -1,10 +1,57 @@
 import React, { Component } from 'react';
 
+import { Button, Table } from 'react-bootstrap';
+
 class Eventos extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            eventos: []
+        }
+    }
+
+    pegarEventos = () => 
+        fetch("http://hdf-api.herokuapp.com/api/eventos")
+            .then(response => response.json())
+            .then(json => this.setState({ eventos: json }));
+
+    componentWillMount() {
+        this.pegarEventos();
+    }
+
+    renderTableRows() {
+        return this.state.eventos.map((content, index) => (
+            <tr key={ index }>
+                <td>{ content.id }</td>
+                <td>{ content.titulo }</td>
+                <td>{ content.data }</td>
+                <td>
+                    <Button bsStyle="warning" bsSize="xsmall">Editar</Button>
+                    <Button bsStyle="danger" bsSize="xsmall">Excluir</Button>
+                </td>
+            </tr>
+        ))
+    }
+    
     render() {
         return (
             <div>
-                Eventos
+                <Button bsStyle="success">Criar Evento</Button>
+                <hr />
+                <Table striped bordered hover responsive>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Título</th>
+                            <th>Data</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.renderTableRows() }
+                    </tbody>
+                </Table>
             </div>
         )
     }
